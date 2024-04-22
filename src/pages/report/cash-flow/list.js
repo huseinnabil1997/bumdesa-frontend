bbe06c18e2d8d383a24d519ccb9219f4d5f58dc1
huseinnabil <seinnabil@gmail.com>
2024-04-22 13:31:11 +0700

    Slicing laporan arus kas

---
+++ src/pages/report/cash-flow/list.js
@@ -0,0 +1,116 @@
+import { useState } from 'react';
+
+// @mui
+import { Card, Table, TableBody, Container, TableContainer } from '@mui/material';
+// hooks
+import useSettings from '../../../hooks/useSettings';
+// layouts
+import Layout from '../../../layouts';
+// components
+import Page from '../../../components/Page';
+import Scrollbar from '../../../components/Scrollbar';
+import { TableHeadCustom, TableNoData, TableSkeleton } from '../../../components/table';
+import AlertDeleteVendor from '../../../components/modal/DeleteVendor';
+// sections
+import { UserTableRow } from '../../../sections/report/profit';
+import { FormProvider } from 'src/components/hook-form';
+import { useForm } from 'react-hook-form';
+import { PROFIT_HEAD } from 'src/utils/constant';
+import { useTheme } from '@mui/material/styles';
+import { StyledButton } from 'src/theme/custom/Button';
+import { Add } from '@mui/icons-material';
+// import { useGetProfit } from 'src/query/hooks/report/profit/useGetProfit';
+import { dataLabaRugi } from './data';
+import { ArusKasHeader } from 'src/sections/report/cash-flow';
+
+// ----------------------------------------------------------------------
+
+LaporanArusKas.getLayout = function getLayout(page) {
+  return <Layout>{page}</Layout>;
+};
+// ----------------------------------------------------------------------
+
+export default function LaporanArusKas() {
+
+  const { themeStretch } = useSettings();
+  const theme = useTheme();
+
+  // const { data, isLoading } = useGetProfit();
+
+  const data = dataLabaRugi;
+  const isLoading = false;
+
+  console.log('data---', data)
+
+  // const [filterName, setFilterName] = useState('');
+  const [alertDelete, setAlertDelete] = useState(null);
+
+  // const handleDeleteRow = (id) => {};
+
+  // const handleEditRow = (row) => {};
+
+  // const handleViewRow = (row) => {};
+
+  const methods = useForm({
+    defaultValues: { unit: null, year: null },
+  });
+
+  const { handleSubmit } = methods;
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Page title="Laporan: Arus Kas">
+      <Container maxWidth={themeStretch ? false : 'lg'}>
+        <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+          <ArusKasHeader />
+        </FormProvider>
+        <Card sx={{ mt: 3 }} elevation={3}>
+          <Scrollbar>
+            <TableContainer sx={{ minWidth: 800, position: 'relative' }}>
+              <Table>
+                <TableHeadCustom
+                  headLabel={PROFIT_HEAD}
+                  rowCount={data?.length}
+                  sx={{ background: theme.palette.grey[200], height: '56px' }}
+                />
+
+                <TableBody>
+                  {!isLoading &&
+                    data.map((row, i) => (
+                      <UserTableRow
+                        key={row.id}
+                        index={i}
+                        row={row}
+                        // onViewRow={() => handleViewRow(row)}
+                      />
+                    ))}
+
+                  {isLoading && <TableSkeleton />}
+                  {!data?.length > 0 && (
+                    <TableNoData
+                      title="Jurnal belum tersedia."
+                      description="Silakan buat jurnal dengan klik tombol di bawah ini."
+                      action={
+                        <StyledButton
+                          sx={{ mt: 2, width: 200 }}
+                          variant="outlined"
+                          startIcon={<Add fontSize="small" />}
+                        >
+                          Buat Jurnal
+                        </StyledButton>
+                      }
+                    />
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Scrollbar>
+        </Card>
+        <AlertDeleteVendor open={!!alertDelete} onClose={() => setAlertDelete(null)} />
+      </Container>
+    </Page>
+  );
+}
