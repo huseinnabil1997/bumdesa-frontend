4e26110e9556c9771ae7ababdfc964ed3319b4d6
huseinnabil <seinnabil@gmail.com>
2024-04-23 13:14:57 +0700

    update email verification page

--- src/pages/auth/verify-email.js
+++ src/pages/auth/verify-email.js
@@ -25,13 +25,18 @@ const RootStyle = styled('div')(({ theme }) => ({
 }));
 
 const ContentStyle = styled('div')(({ theme }) => ({
-  maxWidth: 480,
+  maxWidth: '100vw',
+  width: '100%',
   margin: 'auto',
   display: 'flex',
   minHeight: '100vh',
   flexDirection: 'column',
   justifyContent: 'center',
   padding: theme.spacing(12, 0),
+  backgroundImage: 'url("/image/email-verification.svg")',
+  backgroundSize: 'cover',
+  backgroundRepeat: 'no-repeat',
+  backgroundPosition: 'center',
 }));
 
 // ----------------------------------------------------------------------
@@ -39,11 +44,15 @@ const ContentStyle = styled('div')(({ theme }) => ({
 export default function Login() {
   const [error, setError] = useState('');
   const [isExpired, setIsExpired] = useState(false);
+  const [loading, setLoading] = useState(true);
   const router = useRouter();
 
   const fetchVerifyEmail = async (unit_verify) => {
+    setLoading(false);
     try {
+
       await axiosInstance.post('/business-units/email-verify', { unit_verify });
+      setLoading(false);
       setIsExpired(false);
       setError('');
       setSession(null);
@@ -51,6 +60,7 @@ export default function Login() {
       console.log('error verifyEmail', error);
       setIsExpired(true);
       setError(error?.message);
+      setLoading(false);
     }
   }
 
@@ -61,31 +71,11 @@ export default function Login() {
   return (
     <Page title="Verify Email">
       <RootStyle>
-        <Container maxWidth="sm">
+        {/* <Container> */}
+        {!loading && (
           <ContentStyle>
             {isExpired ? (
               <Box sx={{ maxWidth: 480, mx: 'auto' }}>
-                <Typography variant="h3" paragraph>
-                  {error}
-                </Typography>
-                <Typography sx={{ color: 'text.secondary' }}>
-                  Silahkan hubungi admin BUM Desa Anda untuk mengirimkan kembali link verifikasi.
-                </Typography>
-                <Box display="flex" sx={{ justifyContent: 'center', alignItems: 'center', mt: '10px' }}>
-                  <Image
-                    visibleByDefault
-                    disabledEffect
-                    src="/image/delete_active_unit.svg"
-                    alt="Verify Email"
-                    sx={{ width: '216px', height: '216px' }}
-                  />
-                </Box>
-              </Box>
-            ) : (
-              <Box sx={{ maxWidth: 480, mx: 'auto' }}>
-                <Typography variant="h3" paragraph>
-                  Verifikasi Berhasil!
-                </Typography>
                 <Box
                   display="flex"
                   flexDirection="column"
@@ -94,25 +84,63 @@ export default function Login() {
                   <Image
                     visibleByDefault
                     disabledEffect
-                    src="/image/registration_success.svg"
+                    src="/image/delete_active_unit.svg"
+                    alt="Verify Email"
+                    sx={{ width: '216px', height: '216px', mb: 3 }}
+                  />
+                  <Typography fontSize="24px" fontWeight={700} color="#292929" paragraph>
+                    {error}
+                  </Typography>
+                  <Typography textAlign="center" fontSize="16px" fontWeight={500} color="#666666">
+                    Silahkan hubungi admin BUM Desa Anda untuk mengirimkan kembali link verifikasi.
+                  </Typography>
+                </Box>
+              </Box>
+            ) : (
+              <Box sx={{ maxWidth: 480, mx: 'auto' }}>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  sx={{ justifyContent: 'center', alignItems: 'center', mt: '10px' }}
+                >
+                  <Image
+                    visibleByDefault
+                    disabledEffect
+                    src="/image/email-verification-success.svg"
                     alt="Verifikasi email berhasil"
                     sx={{ width: '216px', height: '216px', mb: 3 }}
                   />
-                  <StyledLoadingButton variant="contained" onClick={() => router.push(PATH_AUTH.login)}>
-                    Masuk
+                  <Typography fontSize="24px" fontWeight={700} color="#292929" paragraph>
+                    Email Anda Telah Terverifikasi
+                  </Typography>
+                  <Typography fontSize="16px" fontWeight={500} color="#666666">
+                    Verifikasi Anda telah berhasil diproses.
+                  </Typography>
+                  <Typography fontSize="16px" fontWeight={500} color="#666666">
+                    Anda dapat menggunakan akun Anda untuk mengakses
+                  </Typography>
+                  <Typography fontSize="16px" fontWeight={500} color="#666666" mb={3}>
+                    semua fitur yang tersedia.
+                  </Typography>
+                  <StyledLoadingButton
+                    sx={{ width: 432, height: 48, fontSize: '16px', fontWeight: 700 }}
+                    variant="contained"
+                    onClick={() => router.push(PATH_AUTH.login)}>
+                    Kembali ke Halaman Login
                   </StyledLoadingButton>
                 </Box>
               </Box>
             )}
           </ContentStyle>
-          {/* <AlertVerifyEmail
+        )}
+        {/* <AlertVerifyEmail
             open={alertVerify}
             onClose={() => {
               setAlertVerify(false);
               router.replace(PATH_AUTH.login)
             }}
           /> */}
-        </Container>
+        {/* </Container> */}
       </RootStyle>
     </Page>
   );
