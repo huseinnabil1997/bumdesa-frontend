9ead20f53446f3b4213e3885daf2801dc79ed638
Ersaad Gisda <ersaad.konsultan@work.bri.co.id>
2024-11-07 16:14:21 +0700

    adjust modal width

--- src/sections/link-umkm/dialogDashboard.js
+++ src/sections/link-umkm/dialogDashboard.js
@@ -30,43 +30,46 @@ const LinkUMKMDialogDashboard = ({ open, onClose }) => {
       onClose={onClose}
       sx={{
         maxWidth: isMobile ? 350 : 900,
-        maxHeight: isMobile ? '608px' : '720px',
         margin: 'auto',
       }}
       fullWidth
+      PaperProps={{
+        sx: {
+          maxWidth: 720,
+        },
+      }}
     >
-      <DialogTitle>
-        <IconButton
-          aria-label="close"
-          onClick={onClose}
-          sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
-          }}
-        >
-          <CloseIcon />
-        </IconButton>
-      </DialogTitle>
-      <DialogContent>
-        <CardMedia
-          component="img"
-          image={isMobile ? '/image/LINKUMKM.gif' : '/image/LINKUMKM_Desktop.gif'}
-          alt="Integrasi LinkUMKM"
-          sx={{
-            width: '100%',
-            maxWidth: isMobile ? 236 : '100%',
-            maxHeight: isMobile ? 480 : '100%',
-          }}
-        />
-      </DialogContent>
+      {/* <DialogTitle>
+       
+      </DialogTitle> */}
+      <IconButton
+        aria-label="close"
+        onClick={onClose}
+        sx={{
+          position: 'absolute',
+          right: 2,
+          top: 2,
+          color: (theme) => theme.palette.grey[500],
+        }}
+      >
+        <CloseIcon />
+      </IconButton>
+      <CardMedia
+        component="img"
+        image={isMobile ? '/image/LINKUMKM.gif' : '/image/LINKUMKM_Desktop.gif'}
+        alt="Integrasi LinkUMKM"
+        sx={{
+          width: '100%',
+          maxWidth: '100%',
+          maxHeight: isMobile ? 500 : '100%',
+        }}
+      />
       <DialogActions sx={{ justifyContent: 'center', px: 2, pb: 2 }}>
         <StyledButton
           onClick={handleClose}
           variant="contained"
           color="primary"
-          sx={{ height: '48px', width: '100%' }}
+          sx={{ height: '48px', width: '100%', mt: 1 }}
         >
           Integrasi LinkUMKM
         </StyledButton>
